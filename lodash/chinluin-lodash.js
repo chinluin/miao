d4738711e964ffd76c8d745ca552f7543b0a397a
qinling <571674417@qq.com>
2019-01-26 21:42:50 +0800

    s1 commit

--- lodash/chinluin-lodash.js
+++ lodash/chinluin-lodash.js
@@ -367,6 +367,303 @@ var chinluin = function() {
     return result   
   }
 
+  function flatten(ary) {
+    let result = []
+    for(let item of ary) {
+      if(Array.isArray(item)) {
+        for(let val of item) {
+          result.push(val)
+        }
+      } else {
+        result.push(item)
+      }
+    }
+    return result
+  }
+
+  function flattenDeep(ary) {
+    let result = []
+    // let result
+    // return ary.reduce((result,item) => {
+    //   if(Array.isArray(item)){
+    //     result.push(...flattenDeep(item))
+    //   } else {
+    //     result.push(item)
+    //   }
+    //   return result
+    // }, [])
+    for(let item of ary) {
+      if(Array.isArray(item)) {
+        result.push(...flattenDeep(item))
+      } else {
+        result.push(item)
+      }
+    }
+    return result
+  }
+
+  function flattenDepth(ary, depth=1) {
+    if(depth === 0)
+      return ary.slice()
+    return ary.reduce((result,item) => {
+      if(Array.isArray(item)){
+        result.push(...flattenDeep(item,depth -1))
+      } else {
+        result.push(item)
+      }
+      return result
+    }, [])
+  }
+
+  function nth(ary, n=0) {
+    return n >= 0 ? ary[n] : ary[ary.length + n]
+  }
+
+  function unzip(ary) {
+    let res = []
+    for(let i=0; i<ary.length; i++) {
+      for(let j=0; j<ary[i].length; j++) {
+        if(res.length < ary[i].length)
+          res.push([])
+        res[j].push(ary[i][j])
+      }
+    }
+    return res
+  }
+
+  function without(ary) {
+    let map = {}
+    for(let i=1; i<arguments.length; i++) {
+      map[arguments[i]] = 1
+    }
+    let res = []
+    for(let i=0; i<ary.length; i++) {
+      if(!(ary[i] in map))
+        res.push(ary[i])
+    }
+    return res
+  },
+
+  function zip(...arys) {
+    let res = []
+    for (let i = 0; i < arys[0].length; i++) {
+        res[i] = arys.map(it => it[i])
+    }
+    return res
+  },
+
+  function forEach(coll,action) {
+    if (Array.isArray(coll)) {
+      for (let i = 0; i < coll.length; i++) {
+        action(coll[i], i, coll)
+      }
+    } else {
+      for (let key in coll) {
+        action(coll[key], key, coll)
+      }
+    }
+    return coll
+  },
+
+  function includes(coll, val, fromIndex = 0) {
+    if(Array.isArray(coll) || typeof coll === 'string') {
+      let idx = coll.indexOf(val)
+      if(idx !== -1 && idx >= fromIndex) {
+        return true
+      } else 
+        return false
+    }
+    if (typeof coll === 'object') {
+      for (let key in coll) {
+        if (coll[key] === val) {
+          return true
+        }
+      }
+      return false
+    }
+  },
+
+  function reduce(coll, f, accu) {
+    for (var val in coll) {
+        if (accu == undefined) {
+            accu = coll[0]
+            continue
+        }
+        accu = f(accu, coll[val], val, coll)
+    }
+    return accu    
+  },
+
+  function map(coll, f) {
+    let res = []
+      for(var idx in coll) {
+        let curr = f(coll[idx], idx, coll)
+        res.push(curr)
+      }
+      return res
+  },
+
+  function reduceRight(coll, f, accu) {
+    for (var i=coll.length-1; i>=0; i--) {
+        if (accu == undefined) {
+            accu = coll[0]
+            continue
+        }
+        accu = f(accu, coll[i], i, coll)
+    }
+    return accu    
+  },
+
+  function size(coll) {
+    if (Array.isArray(coll) || typeof coll == 'string') {
+        return coll.length
+    }
+    if (typeof coll == 'object') {
+      let c = 0
+      for(var key in coll)
+        c++
+      return c
+    }
+  },
+
+  function eq(val, other) {
+    if(Number.isNaN(val) && Number.isNaN(other)) {
+      return true
+    }
+    if(val === other)
+      return true
+     else 
+      return false
+  },
+
+  function gt(val, other) {
+    if(val > other)
+      return true
+      else 
+        return false
+  },
+
+  function gte(val, other) {
+    if(val >= other)
+      return true
+      else 
+        return false
+  },
+
+  function isArguments(val) {
+    return Object.prototype.toString.call(val) === '[object Arguments]'
+  },
+
+  function isArray(val) {
+    return Object.prototype.toString.call(val) === '[object Array]'
+  },
+
+  function isArrayBuffer(val) {
+    return Object.prototype.toString.call(val) === '[object ArrayBuffer]'    
+  },
+
+  function isArrayLike(val) {
+    return Object.prototype.toString.call(val) !== "[object Function]" && val.length >= 0 &&  val.length <= Number.MAX_SAFE_INTEGER
+  },
+
+  function isArrayLikeObject(val) {
+    return Object.prototype.toString.call(val) !== "[object Function]" && typeof val === 'object' && val.length >= 0 &&  val.length <= Number.MAX_SAFE_INTEGER
+  },
+
+  function isBoolean(val) {
+    return Object.prototype.toString.call(val) === '[object Boolean]'
+  },
+
+  function isDate(val) {
+    return Object.prototype.toString.call(val) === '[object Date]'
+  },
+
+  function isElement(val) {
+    return Object.prototype.toString.call(val) === '[object HTMLBodyElement]'
+  },
+
+  function isEmpty(val) {
+    if(val === null || val.length === undefined)
+      return true
+      else 
+        return size(val) === 0
+  },
+
+  function isError(val) {
+    return Object.prototype.toString.call(val) === '[object Error]'
+  },
+
+  function isFinite(val) {
+    return typeof val === "number" && val <= Number.MAX_VALUE && val >= Number.MIN_VALUE
+  },
+
+  function isFunction(val) {
+    return Object.prototype.toString.call(val) === '[object Function]' 
+  },
+
+  function isInteger(val) {
+    return typeof val === 'number' && isFinite(val)
+  },
+
+  function isLength(val) {
+    return isArrayLike(val) && isInteger(val)
+  },
+
+  function isMap(val) {
+    return Object.prototype.toString.call(val) === '[object Map]'
+  },
+
+  function isNaN(val) {
+    return (val !== val && typeof val === 'number') || (typeof val === "object" && val.toString() === "NaN")
+  },
+
+  function isNull(val) {
+    return Object.prototype.toString.call(val) === '[object Null]'
+  },
+
+  function isUndefined(val) {
+    return Object.prototype.toString.call(val) === '[object Undefined]'
+  },
+
+  function isNumber(val) {
+    return Object.prototype.toString.call(val) === '[object Number]'
+  },
+
+  function isObject(val) {
+    return Object.prototype.toString.call(val) === '[object Object]'
+  },
+
+  function isString(val) {
+    return Object.prototype.toString.call(val) === '[object String]'
+  },
+
+  function lt(val, other) {
+    return val < other
+  },
+
+  function lte(val, other) {
+    return val <= other
+  },
+
+  function toArray(val) {
+    let res = []
+    if(typeof val === 'object' || typeof val === 'string') {
+      for(let key in val)
+        res.push(val[key])
+    }
+    return res
+  },
+
+  function toFinite(val) {
+    if(val === Infinity)
+      return 1.7976931348623157e+308
+      else if(val === -Infinity)
+      return -1.7976931348623157e+308
+        else
+          return Number(val)
+  },
+
+
 
   return {
     chunk: chunk,
@@ -401,6 +698,46 @@ var chinluin = function() {
     union: union,
     uniq: uniq,
     xor: xor,
+    flatten,
+    flattenDeep,
+    flattenDepth,
+    nth,
+    unzip,
+    zip,
+    forEach,
+    includes,
+    reduce,
+    map,
+    reduceRight,
+    size,
+    eq,
+    gt,
+    gte,
+    isArguments,
+    isArray,
+    isArrayBuffer,
+    isArrayLike,
+    isArrayLikeObject,
+    isBoolean,
+    isDate,
+    isElement,
+    isEmpty,
+    isError,
+    isFinite,
+    isFunction,
+    isInteger,
+    isLength,
+    isMap,
+    isNaN,
+    isNull,
+    isUndefined,
+    isNumber,
+    isObject,
+    isString,
+    lt,
+    lte,
+    toArray,
+    toFinite,
   }
 
 
